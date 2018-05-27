2067a811ccd505d59060ccc6f205de8c49a662d1
peilingkuang <1589330364@qq.com>
2018-05-27 15:00:00 +0800

    第二版

--- trade-get.py
+++ trade-get.py
@@ -7,7 +7,7 @@ import os
 
 def main(argv):
     if not argv or len(argv) != 2:
-        print "usage:%s %s" % __file__, "201805192216"
+        print "usage:python %s %s" % (__file__, "201805192216")
         return
     exportOrderListPath = "ExportOrderList%s.csv" % argv[1]
     exportOrderDetailListPath = "ExportOrderDetailList%s.csv" % argv[1]
@@ -16,7 +16,7 @@ def main(argv):
     if not os.path.exists(exportOrderDetailListPath) or not os.path.exists(exportOrderListPath):
         print "can not find file %s or %s" % (exportOrderListPath, exportOrderDetailListPath)
 
-    df = pandas.read_csv(exportOrderListPath, encoding="gbk", header=0, sep=',',
+    df = pandas.read_csv(exportOrderListPath, encoding="gbk", header=0, sep=',', na_values='NULL',
                          names=["订单编号", "买家会员名", "买家支付宝账号", "买家应付货款", "买家应付邮费", "买家支付积分", "总金额", "返点积分", "买家实际支付金额",
                                 "买家实际支付积分", "订单状态", "买家留言", "收货人姓名", "收货地址 ", "运送方式", "联系电话 ", "联系手机", "订单创建时间",
                                 "订单付款时间 ", "宝贝标题 ", "宝贝种类 ", "物流单号 ", "物流公司", "订单备注", "宝贝总数量", "店铺Id", "店铺名称", "订单关闭原因",
@@ -27,33 +27,40 @@ def main(argv):
     # df = pandas.read_csv(exportOrderListPath, encoding="gbk")
     # print df.ix[:, "订单编号"]
 
-    order_list = df.ix[:,
-                 ["订单编号", "买家会员名", "收货人姓名", "收货地址 ", "联系手机", "买家留言", "订单备注", "修改后的收货地址", "买家实际支付金额", "订单付款时间 ", "异常信息"]]
+    order_list = df.loc[df["订单状态"] == u"买家已付款，等待卖家发货",
+                        ["订单编号", "买家会员名", "收货人姓名", "收货地址 ", "联系手机", "买家留言", "订单备注", "修改后的收货地址", "买家实际支付金额", "订单付款时间 ",
+                         "异常信息"]]
+
+    # 修改字段
+    order_list["联系手机"] = order_list["联系手机"].str.replace(ur"'", "")
+    # print order_list["联系手机"]
+
     order_list["收货人信息"] = order_list["收货人姓名"].map(unicode) + ',' + order_list["联系手机"].map(unicode) + ',' + order_list[
         "收货地址 "].map(unicode)
     order_list.drop(order_list.columns[[2, 3, 4]], axis=1, inplace=True)
     # print order_list
 
-    df2 = pandas.read_csv(exportOrderDetailListPath, encoding="gbk", header=0, sep=',',
+    df2 = pandas.read_csv(exportOrderDetailListPath, encoding="gbk", header=0, sep=',', na_values='NULL',
                           names=["订单编号", "标题", "价格", "购买数量", "外部系统编号", "商品属性", "套餐信息", "备注", "订单状态", "商家编码"])
 
-    detail_list = df2.ix[:, ["订单编号", "商品属性", "购买数量", "订单状态", "标题"]]
-
     # 过滤订单关闭的
-    # detail_list(True-df["订单状态"].isin(["交易关闭"]))
-    detail_list = detail_list[detail_list["订单状态"] != u"交易关闭"]
-    detail_list["拣货"] = '(' + detail_list["商品属性"].map(unicode) + '*' + detail_list["购买数量"].map(unicode) + ')'
+    detail_list = df2.loc[df2["订单状态"] == u"买家已付款，等待卖家发货", ["订单编号", "商品属性", "购买数量", "订单状态", "标题"]]
+    # detail_list = detail_list[detail_list["订单状态"] != u"交易关闭"]
+
+    # 修改字段
+    detail_list["商品属性"] = detail_list["商品属性"].str.replace(ur"颜色分类：", "", regex=True)
+
+    # detail_list["拣货"] = '(' + detail_list["商品属性"].map(unicode) + '*' + detail_list["购买数量"].map(unicode) + ')'
+    detail_list["拣货"] = detail_list["商品属性"].map(unicode) + '*' + detail_list["购买数量"].map(unicode)
     detail_list.drop(detail_list.columns[[1, 2]], axis=1, inplace=True)
-    detail_list = detail_list.groupby("订单编号").aggregate(lambda x: '\n'.join(list(x)))
-    # print detail_list
-    # dg = detail_list.groupby("订单编号")
-    # print dg
+    detail_list = detail_list.groupby("订单编号").aggregate(lambda x: u'，\n'.join(list(x)))
 
-    out_list = pandas.merge(order_list, detail_list, how='outer', on="订单编号")
-    out = out_list.groupby("买家会员名").aggregate(lambda x: set(x))
-    out = out.sort_values(by="订单付款时间 ")
-    columns = ["收货人信息", "拣货", "买家留言", "订单备注", "订单状态", "标题", "买家实际支付金额", "订单编号", "修改后的收货地址", "订单付款时间 ", "异常信息"]
+    out_list = pandas.merge(order_list, detail_list, how='outer', on="订单编号").fillna("null")
+    out_list["买家实际支付金额"] = out_list["买家实际支付金额"].map(str)
+    out = out_list.groupby("买家会员名").aggregate(lambda x: u'，\n'.join(set(x)))
+    # out = out.sort_values(by="订单付款时间 ")
 
+    columns = ["收货人信息", "拣货", "买家留言", "订单备注", "标题", "买家实际支付金额", "订单编号", "修改后的收货地址", "订单付款时间 ", "异常信息"]
     out.to_csv(resultPath, encoding="gbk", columns=columns)
 
     # out.reindex("订单付款时间 ")
